1664174bf8abb0dc4a3c1c9b2f71529d7245a1e7
Sokny Norn <sokny.norn@pnc.lan>
2019-09-01 14:59:02 +0700

    php homework

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  <strong>Login</strong>
+                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label for="inputUsername3" class="col-sm-3 control-label">
+                            Username</label>
+                        <div class="col-sm-9">
+                            <input type="Username" class="form-control" id="inputUsernamel3" placeholder="Username" required="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-3 control-label">
+                            Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                           
+                        </div>
+                    </div>
+                    <div class="form-group last">
+                        <div class="col-sm-offset-3 col-sm-9">
+                            <button type="submit" class="btn btn-success btn-sm">
+                                Login</button>
+                                 <button type="reset" class="btn btn-default btn-sm">
+                                Register</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
